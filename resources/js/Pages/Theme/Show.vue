06b840e899fb96ccac17b7cad8e91c15a6663611
Paul.Inglsmit <paul.inglsmit@gmail.com>
2023-08-08 23:14:40 +0300

    #29 Complains is_not_solved_complaint mark

--- resources/js/Pages/Theme/Show.vue
+++ resources/js/Pages/Theme/Show.vue
@@ -21,47 +21,52 @@
                     </div>
                 </div>
                 <div class="p-4 w-5/6">
-                    <div>
+                    <div class="mb-2">
                         <p class="text-sm">{{ message.time }}</p>
                     </div>
-                    <div class="mb-4">
-                        <p v-html="message.content"></p>
-                    </div>
-                    <div class="mb-4 flex items-center justify-end w-full">
-
-                        <div class="mr-4">
-                            <a href="#" @click.prevent="openComplaint(message)" class="text-sm text-white rounded-lg border border-red-500 inline-block p-2 text-center text-red-500">
-                                Complain
-                            </a>
+                    <div>
+                        <div v-if="message.is_not_solved_complaint">
+                            <p class="w-full bg-red-50 p-2">Your complain on review</p>
                         </div>
-
-                        <div class="mr-4">
-                            <a href="#" @click.prevent="answer(message)" class="text-sm text-white rounded-lg bg-indigo-600 border border-indigo-700 inline-block p-2 text-center">
-                                Reply
-                            </a>
+                        <div class="mb-4">
+                            <p v-html="message.content"></p>
                         </div>
+                        <div class="mb-4 flex items-center justify-end w-full">
 
-                        <div class="mr-4">
-                            <a href="#" @click.prevent="quote(message.content)" class="text-sm text-white rounded-lg bg-sky-600 border border-sky-700 inline-block p-2 text-center">
-                                Quote
-                            </a>
-                        </div>
+                            <div class="mr-4">
+                                <a href="#" @click.prevent="openComplaint(message)" class="text-sm text-red-600 rounded-lg border border-red-500 inline-block p-2 text-center text-red-500">
+                                    Complain
+                                </a>
+                            </div>
 
-                        <div class="flex items-center">
-                            <span class="mr-2">
-                                {{ message.likes }}
-                            </span>
-                            <a @click.prevent="toggleLike(message)" href="#">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                     :class="[message.is_liked ? 'fill-sky-600' : '', 'w-6 h-6 stroke-sky-600']">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                                </svg>
-                            </a>
+                            <div class="mr-4">
+                                <a href="#" @click.prevent="answer(message)" class="text-sm text-white rounded-lg bg-indigo-600 border border-indigo-700 inline-block p-2 text-center">
+                                    Reply
+                                </a>
+                            </div>
+
+                            <div class="mr-4">
+                                <a href="#" @click.prevent="quote(message.content)" class="text-sm text-white rounded-lg bg-sky-600 border border-sky-700 inline-block p-2 text-center">
+                                    Quote
+                                </a>
+                            </div>
+
+                            <div class="flex items-center">
+                                <span class="mr-2">
+                                    {{ message.likes }}
+                                </span>
+                                <a @click.prevent="toggleLike(message)" href="#">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                         :class="[message.is_liked ? 'fill-sky-600' : '', 'w-6 h-6 stroke-sky-600']">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                                    </svg>
+                                </a>
+                            </div>
                         </div>
-                    </div>
-                    <div class="flex items-center" v-if="message.is_complaint">
-                        <input v-model="message.body" class="w-5/6 p-2 rounded-lg rounded-r-none border border-gray-300" placeholder="Your complain" type="text" name="" id="" />
-                        <a @click.prevent="complaint(message)" href="#" class="block p-2 rounded-lg rounded-l-none w-1/6 text-center bg-red-800 border-gray-300 text-white">Send</a>
+                        <div class="flex items-center" v-if="message.is_complaint">
+                            <input v-model="message.body" class="w-5/6 p-2 rounded-lg rounded-r-none border border-gray-300" placeholder="Your complain" type="text" name="" id="" />
+                            <a @click.prevent="complaint(message)" href="#" class="block p-2 rounded-lg rounded-l-none w-1/6 text-center bg-red-800 border-gray-300 text-white">Send</a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -142,7 +147,7 @@ export default {
                 body: message.body,
                 theme_id: message.theme_id
             }).then( () => {
-                message.body = ''
+                message.is_not_solved_complaint = res.data.is_not_solved_complaint
             })
         },
     },

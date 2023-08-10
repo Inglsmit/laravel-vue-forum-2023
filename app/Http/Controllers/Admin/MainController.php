3f1be57bcd3c06bc67b5385e0da3f7d7c6c53452
Paul.Inglsmit <paul.inglsmit@gmail.com>
2023-08-10 14:05:24 +0300

    #36 Admin panel - basic statistics

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -3,12 +3,35 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Branch;
+use App\Models\Message;
+use App\Models\Theme;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
-        return inertia('Admin/Main/Index');
+        $stats = [
+            [
+                'title' => 'Branches',
+                'count' => Branch::count(),
+            ],
+            [
+                'title' => 'Themes',
+                'count' => Theme::count()
+            ],
+            [
+                'title' => 'Messages',
+                'count' => Message::count()
+            ],
+            [
+                'title' => 'Users',
+                'count' => User::count()
+            ],
+        ];
+
+        return inertia('Admin/Main/Index', compact('stats'));
     }
 }
--- resources/js/Pages/Admin/Main/Index.vue
+++ resources/js/Pages/Admin/Main/Index.vue
@@ -3,6 +3,15 @@
         <div class="flex items-center mb-8">
             <h3 class="text-xl mr-4">Statistics</h3>
         </div>
+        <div>
+            <div v-if="stats"  class="flex justify-between text-center">
+                <div v-for="stat in stats">
+                    <h3>{{ stat.title }}</h3>
+                    <p>{{ stat.count }}</p>
+                </div>
+            </div>
+            <div></div>
+        </div>
     </div>
 </template>
 
@@ -13,7 +22,7 @@ export default {
     name: "Index",
 
     props: [
-        'sections'
+        'stats'
     ],
 
     components: {
